95e9cd0f904f17af4bdaed7f31a9b8fcad1a0e98
FanRende <zxcj04@gmail.com>
2021-12-02 12:15:50 +0800

    Initial Commit

---
+++ PhotoPoof/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoPoof
+//
+//  Created by FanRende on 2021/12/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoPoof/PhotoPoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPoofApp.swift
+//  PhotoPoof
+//
+//  Created by FanRende on 2021/12/2.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
